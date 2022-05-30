12a99b1f16e22832b3e4e591dce3b4e96c7460bb
Aleksandr Rogozhnikov <87545758+alekrogozhnikov@users.noreply.github.com>
2022-05-30 23:31:14 +0300

    Update views.sql

--- src/views.sql
+++ src/views.sql
@@ -1,1 +1,17 @@
+-- Создаем представления в схеме analysis
 
+CREATE OR REPLACE view orderitems as
+	select *
+	from production.orderitems;
+CREATE OR REPLACE view orders as
+	select *
+	from production.orders;
+CREATE OR REPLACE view orderstatuses as
+	select *
+	from production.orderstatuses;
+CREATE OR REPLACE view orderstatuslog as
+	select *
+	from production.orderstatuslog;
+CREATE OR REPLACE view products as
+	select *
+	from production.products;
