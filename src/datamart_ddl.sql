17c7ad2f84560b06c71661acd314b0673ff52140
Aleksandr Rogozhnikov <87545758+alekrogozhnikov@users.noreply.github.com>
2022-05-31 20:28:23 +0300

    Update datamart_ddl.sql

--- src/datamart_ddl.sql
+++ src/datamart_ddl.sql
@@ -1,1 +1,9 @@
-
+CREATE table analysis.dm_rfm_segments(
+	user_id int4 NOT null PRIMARY key,
+	recency int2 NOT null,
+	frequency int2 NOT null,
+	monetary_value int2 NOT null,
+	CONSTRAINT dm_rfm_segments_recency_check CHECK((recency > (0)::numeric) and (recency <= (5)::numeric)),
+	CONSTRAINT dm_rfm_segments_frequency_check CHECK((recency > (0)::numeric) and (recency <= (5)::numeric)),
+	CONSTRAINT dm_rfm_segments_monetary_check CHECK((recency > (0)::numeric) and (recency <= (5)::numeric))
+);
