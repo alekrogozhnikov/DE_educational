2ecf5175632fdf4cccf26208775c7b6f6a5aa622
Aleksandr Rogozhnikov <87545758+alekrogozhnikov@users.noreply.github.com>
2022-05-31 21:40:54 +0300

    Update datamart_query.sql

--- src/datamart_query.sql
+++ src/datamart_query.sql
@@ -3,3 +3,17 @@ insert into dm_rfm_segments (user_id, recency, frequency, monetary_value)
 	from tmp_rfm_recency 
 		left join tmp_rfm_frequency using(user_id)
 		left join tmp_rfm_monetary_value using(user_id)
+	
+	
+user_id|recency|frequency|monetary_value|
+-------+-------+---------+--------------+
+      0|      3|        5|             5|
+      1|      4|        3|             4|
+      2|      3|        3|             3|
+      3|      3|        3|             2|
+      4|      3|        3|             2|
+      5|      4|        5|             5|
+      6|      1|        2|             2|
+      7|      5|        1|             2|
+      8|      4|        1|             2|
+      9|      4|        3|             2|
