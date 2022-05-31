3fe786a08cdad5d78dc97511c9f401a54b328978
Aleksandr Rogozhnikov <87545758+alekrogozhnikov@users.noreply.github.com>
2022-05-31 21:20:21 +0300

    Update tmp_rfm_frequency.sql

--- src/tmp_rfm_frequency.sql
+++ src/tmp_rfm_frequency.sql
@@ -1,6 +1,5 @@
-select user_id,
-	count(order_id) cnt_orders,
-	ntile(5) over(order by count(order_id),user_id) frequency
-from orders
-group by user_id
-order by cnt_orders, user_id
+insert into tmp_rfm_frequency (user_id, frequency)
+	select user_id,
+		ntile(5) over(order by count(order_id),user_id) frequency
+	from orders
+	group by user_id
