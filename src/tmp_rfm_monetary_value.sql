c45952b6c002c83df4b50b0f603b937bbdd4d698
Aleksandr Rogozhnikov <87545758+alekrogozhnikov@users.noreply.github.com>
2022-05-31 21:19:31 +0300

    Update tmp_rfm_monetary_value.sql

--- src/tmp_rfm_monetary_value.sql
+++ src/tmp_rfm_monetary_value.sql
@@ -1,6 +1,5 @@
+insert into tmp_rfm_monetary_value (user_id, monetary_value)
 select user_id,
-	sum(cost) total_orders,
 	ntile(5) over(order by sum(cost), user_id) monetary_value
 from orders
 group by user_id
-order by total_orders, user_id
