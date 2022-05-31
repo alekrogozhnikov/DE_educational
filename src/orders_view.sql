8feb85132fccde0b0f0f3842f5e462456f6ac69d
Aleksandr Rogozhnikov <87545758+alekrogozhnikov@users.noreply.github.com>
2022-06-01 00:29:13 +0300

    Update orders_view.sql

--- src/orders_view.sql
+++ src/orders_view.sql
@@ -1,6 +1,7 @@
-CREATE OR REPLACE view orders as
-	select o.*, (select max(status_id)
-				from production.orderstatuslog ol
-				where o.order_id = ol.order_id
-				group by order_id) status
-	from production.orders o;
+create or replace view orders as
+select o.*, olog.status
+from production.orders o
+left join (
+	select distinct(order_id), first_value(status_id) over(partition by order_id order by dttm desc) status
+	from production.orderstatuslog
+	) olog on o.order_id = olog.order_id 
