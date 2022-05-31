27f8ea7b37090040bd5281c6c14bff8c37f92563
Aleksandr Rogozhnikov <87545758+alekrogozhnikov@users.noreply.github.com>
2022-05-31 21:21:34 +0300

    Update tmp_rfm_recency.sql

--- src/tmp_rfm_recency.sql
+++ src/tmp_rfm_recency.sql
@@ -1,6 +1,5 @@
-select user_id,
-	MAX(order_ts) lasttime_orders,
-	ntile(5) over(order by MAX(order_ts), user_id) recency
-from orders
-group by user_id
-order by lasttime_orders, user_id
+insert into tmp_rfm_recency (user_id, recency)
+	select user_id,
+		ntile(5) over(order by MAX(order_ts), user_id) recency
+	from orders
+	group by user_id
