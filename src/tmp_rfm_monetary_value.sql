d93ff8d65f6ac12dcc7ffa6a864ce37beb8fb6c3
Aleksandr Rogozhnikov <87545758+alekrogozhnikov@users.noreply.github.com>
2022-06-02 13:29:38 +0300

    Update tmp_rfm_monetary_value.sql

--- src/tmp_rfm_monetary_value.sql
+++ src/tmp_rfm_monetary_value.sql
@@ -1,5 +1,6 @@
-insert into tmp_rfm_monetary_value (user_id, monetary_value)
+insert into analysis.tmp_rfm_monetary_value(user_id, monetary_value)
 	select user_id,
 		ntile(5) over(order by sum(cost), user_id) monetary_value
-	from orders
+	from analysis.orders
+	where date_part('year', order_ts) >= 2021 and status = 4
 	group by user_id
