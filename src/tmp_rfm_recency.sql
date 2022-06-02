38d21a56d27fb4fa0f8750e55111000dbccdc262
Aleksandr Rogozhnikov <87545758+alekrogozhnikov@users.noreply.github.com>
2022-06-02 13:19:50 +0300

    Update tmp_rfm_recency.sql

--- src/tmp_rfm_recency.sql
+++ src/tmp_rfm_recency.sql
@@ -1,5 +1,6 @@
-insert into tmp_rfm_recency (user_id, recency)
-	select user_id,
-		ntile(5) over(order by MAX(order_ts), user_id) recency
-	from orders
-	group by user_id
+insert into tmp_rfm_recency (user_id, recency) 
+	select user_id, 
+		ntile(5) over(order by MAX(order_ts), user_id) recency 
+	from analysis.orders
+	where date_part('year', order_ts) >= 2021 and status = 4
+	group by user_id 
