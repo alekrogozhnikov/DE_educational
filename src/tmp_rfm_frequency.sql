06dd29a18a5da02e66dafdfaf68c9205143496f4
Aleksandr Rogozhnikov <87545758+alekrogozhnikov@users.noreply.github.com>
2022-06-04 15:33:26 +0300

    Update tmp_rfm_frequency.sql

--- src/tmp_rfm_frequency.sql
+++ src/tmp_rfm_frequency.sql
@@ -1,6 +1,6 @@
-insert into analysis.tmp_rfm_frequency (user_id, frequency) 
-	select user_id, 
-		ntile(5) over(order by count(order_id),user_id) frequency 
-	from analysis.orders
-	where date_part('year', order_ts) >= 2021 and status = 4
-	group by user_id
+insert into analysis.tmp_rfm_frequency (user_id, frequency)
+	select u.id user_id,
+			ntile(5) over(order by count(o.order_id)) as frequency
+	from analysis.users u 
+		left join analysis.orders o on o.user_id = u.id and o.status = 4
+	group by 1
